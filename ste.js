7c7d3698892766a8e242577e0579913647134832
LebCit <lebcit@gmail.com>
2024-06-26 16:41:12 +0300

    Add support for nested each blocks and dot notation

--- ste.js
+++ ste.js
@@ -176,30 +176,52 @@ class STE {
 			}
 		})
 
-		const eachRegex = /{{#each\s+(.+?)}}([\s\S]*?){{\/each}}/g
+		const resolveDotNotation = (value, dataObject) => {
+			return value
+				.split(".")
+				.reduce((obj, key) => (obj && obj[key] !== "undefined" ? obj[key] : undefined), dataObject)
+		}
 
-		content = content.replace(eachRegex, (match, arrayKey, innerContent) => {
-			const arrayValue = dataObject[arrayKey.trim()]
-			if (Array.isArray(arrayValue)) {
-				return arrayValue
-					.map((item) => {
-						let itemContent = innerContent
-						if (typeof item === "object") {
-							for (const key in item) {
-								if (item.hasOwnProperty(key)) {
-									itemContent = itemContent.replace(new RegExp(`{{${key}}}`, "g"), item[key])
+		const processEach = (content, dataObject, eachCounter = 0) => {
+			const eachRegex = new RegExp(
+				`{{#each${eachCounter ? eachCounter : ""}\\s+(.+?)}}([\\s\\S]*?){{\\/each${
+					eachCounter ? eachCounter : ""
+				}}}`,
+				"g"
+			)
+
+			return content.replace(eachRegex, (match, arrayKey, innerContent) => {
+				const arrayValue = resolveDotNotation(arrayKey.trim(), dataObject)
+
+				if (Array.isArray(arrayValue)) {
+					return arrayValue
+						.map((item) => {
+							let itemContent = innerContent
+
+							if (typeof item === "object") {
+								for (const key in item) {
+									if (item.hasOwnProperty(key)) {
+										const itemRegex = new RegExp(`{{${key}}}`, "g")
+										itemContent = itemContent.replace(itemRegex, item[key])
+									}
 								}
+							} else {
+								itemContent = itemContent.replace(/{{this}}/g, item)
 							}
-						} else {
-							itemContent = itemContent.replace(/{{this}}/g, item)
-						}
-						return itemContent
-					})
-					.join("")
-			} else {
-				return ""
-			}
-		})
+
+							// Recursively process nested each blocks
+							itemContent = processEach(itemContent, item, eachCounter + 1)
+
+							return itemContent
+						})
+						.join("")
+				} else {
+					return ""
+				}
+			})
+		}
+
+		content = processEach(content, dataObject)
 
 		return content
 	}
