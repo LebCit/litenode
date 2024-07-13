a8613b912ce199a3e706ab0899bbc480c34c81ec
LebCit <lebcit@gmail.com>
2024-07-14 01:21:02 +0300

    docs(paginator): add JSDoc comments and enhance pagination logic
    - Added JSDoc comments to paginator function for better documentation and clarity.
    - Improved pagination logic to include previous and next items in the result object.
    - Ensured consistent data types for page and per_page properties.
    - Updated result object to provide more comprehensive pagination information.

--- core/utils/paginator.js
+++ core/utils/paginator.js
@@ -1,17 +1,28 @@
+/**
+ * Paginates an array of items and returns a paginated result object with previous and next items.
+ * @param {Array<any>} items - The array of items to paginate.
+ * @param {number} current_page - The current page number (default is 1).
+ * @param {number} per_page_items - Number of items per page (default is 10).
+ * @returns {Object} - A paginated result object containing page information, previous and next items, and paginated data.
+ */
 export function paginator(items, current_page = 1, per_page_items = 10) {
 	let page = current_page,
 		per_page = per_page_items,
 		offset = (page - 1) * per_page,
-		paginatedItems = items.slice(offset, offset + per_page),
-		total_pages = Math.ceil(items.length / per_page)
+		paginatedItems = items.slice(offset, offset + per_page), // Get items for the current page
+		total_pages = Math.ceil(items.length / per_page), // Calculate total number of pages
+		prev_item = offset > 0 ? items[offset - 1] : null, // Previous item or null if on the first page
+		next_item = offset + per_page < items.length ? items[offset + per_page] : null // Next item or null if on the last page
 
 	return {
-		page: page,
-		per_page: per_page,
-		prev_page: page - 1 > 0 ? page - 1 : null,
-		next_page: total_pages > page ? page + 1 : null,
-		total_files: items.length,
-		total_pages: total_pages,
-		data: paginatedItems,
+		page: Number(page), // Current page number
+		per_page: per_page, // Number of items per page
+		prev_page: page > 1 ? Number(page) - 1 : null, // Previous page number or null if on the first page
+		next_page: page < total_pages ? Number(page) + 1 : null, // Next page number or null if on the last page
+		prev_item: prev_item, // Previous item in the array
+		next_item: next_item, // Next item in the array
+		total_items: items.length, // Total number of items
+		total_pages: total_pages, // Total number of pages
+		data: paginatedItems, // Items for the current page
 	}
 }
